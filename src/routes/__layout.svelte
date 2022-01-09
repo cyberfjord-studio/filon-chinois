851326371d862ba04e61139d77cfc594e275d754
Joshua L.J. Vachon <joshua.vachon@cyberfjord.studio>
2022-01-08 19:21:27 -0500

    Initialisation

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,11 @@
+<script>
+  import '../main.css'
+</script>
+
+<div class="min-h-screen h-screen max-h-screen w-screen flex flex-row">
+  <nav class="w-24 h-screen bg-gray-50 z-40 shadow-2xl">
+
+  </nav>
+  <main class="grow"><slot/></main>
+</div>
+
