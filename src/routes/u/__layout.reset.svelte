8f8ad296ad2e52da08fa5ec1db3b4c4d33befacd
Joshua L.J. Vachon <joshua.vachon@cyberfjord.studio>
2022-01-18 12:10:11 -0500

    Without api

--- src/routes/u/__layout.reset.svelte
+++ src/routes/u/__layout.reset.svelte
@@ -94,20 +94,7 @@ import { onMount } from 'svelte';
     
   }
 
-  onMount(()=>{
-    
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-    s1.async=true;
-    s1.src='https://embed.tawk.to/6161d7e786aee40a5735bfc1/1fhj36obd';
-    s1.charset='UTF-8';
-    s1.setAttribute('crossorigin','*');
-    s0.parentNode.insertBefore(s1,s0);
-    })();
-
-
-  })
+ 
 </script>
 
 <svelte:window on:mouseup={fabtrad} on:mousedown={menageSelection}/>
